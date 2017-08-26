802e39a319d1bb33620ed5bb150ac094ce78bba9
SenaniM <maheshika@hsenidlanka.com>
2017-08-27 00:53:48 +0530

    Initialized Robot

---
+++ Robot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Robot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot();
+
+            robot.Strength = 75;
+            robot.Life = 100;
+
+            robot.shoot();
+        }
+    }
+}
---
+++ Robot/Robot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robot
+{
+    class Robot
+    {
+        private int life, strenght;
+
+        public int Strength {
+            get
+            {
+                return strenght;
+            }
+            set
+            {
+                strenght = value;
+            }
+        }
+
+        public int Life
+        {
+            get
+            {
+                return life;
+            }
+            set
+            {
+                life = value;
+            }
+        }
+    }
+}
